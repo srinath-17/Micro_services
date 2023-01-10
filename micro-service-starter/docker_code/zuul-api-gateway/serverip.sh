e69b32529febc4a5177892f1b4aed09da48525e8
srinath-17 <85933521+srinath-17@users.noreply.github.com>
2023-01-10 11:14:46 +0530

    Update serverip.sh

--- micro-service-starter/docker_code/zuul-api-gateway/serverip.sh
+++ micro-service-starter/docker_code/zuul-api-gateway/serverip.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 home=/var/lib/jenkins/workspace/zuul-api-gateway/micro-service-starter/docker_code/zuul-api-gateway/src/main/resources
+chmod +x $(home)/application.yml
 
 # Get the public IP address
 PUBLIC_IP=$(curl -s https://api.ipify.org)
